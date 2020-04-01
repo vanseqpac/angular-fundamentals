6b33fa74bd422f19a736517d80dae0e99f6e8bed
Vanessa Sequeira <vssequeira@gmail.com>
2020-04-01 17:24:13 -0500

    Committing Ex:3 NG Basics workshop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-box-of-donuts size="5"></app-box-of-donuts>
+    <app-box-of-donuts size="2"></app-box-of-donuts>
     <!-- Use the NgFor Directive to iterate over the array of numbers.
       Bold the number if it is even. -->
   `
--- src/app/box-of-donuts.component.ts
+++ src/app/box-of-donuts.component.ts
@@ -4,11 +4,19 @@ import { Donut } from './models/donut.interface';
 @Component({
   selector: 'app-box-of-donuts',
   template: `
-    <!-- Show "small box" when 4 or less, "large box" when 8 or more, otherwise just "box" -->
     <p>Size: {{ size }}</p>
+
+    <!-- Show "small box" when 4 or less, "large box" when 8 or more, otherwise just "box" -->
+    <p *ngIf="size <= 4">Small Box</p>
+    <p *ngIf="size >= 8">Large Box</p>
+    <p *ngIf="size > 4 && size < 8">Box</p>
+
     <!-- Show "box full" when the number of donuts equals the size of the box -->
+    <!-- Vanessa: === not only checks the value but also type of two variable -->
+    <p *ngIf="donuts.length==size">Box Full</p>
+
     <!-- Show all donuts in the box using NgFor -->
-    <app-donut [donut]="donuts[0]"></app-donut>
+    <app-donut *ngFor="let donut of donuts" [donut]="donut"></app-donut>
   `
 })
 export class BoxOfDonutsComponent {
@@ -18,4 +26,5 @@ export class BoxOfDonutsComponent {
   ];
 
   @Input() size = 6;
+
 }
